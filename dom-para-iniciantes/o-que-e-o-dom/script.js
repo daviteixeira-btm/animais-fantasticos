cf8badaccd2f3f16f89fd924652fbcd7d8d24c84
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-04-18 18:11:54 -0300

    DOM para iniciantes - exercicio

---
+++ dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -0,0 +1,13 @@
+// Retorne o url da página atual utilizando o objeto window
+const urlPagina = window.location.href;
+console.log(urlPagina)
+
+// Seleciona o primeiro elemento da página que
+// possui a classe ativo
+const elementoAtivo = document.querySelector('.ativo')
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+
+// Retorne a largura da janela
+const largura = window.innerWidth;
